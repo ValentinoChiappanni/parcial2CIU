3c4bf7ac6bc5396cbbe3ecaeec6226eaa64241ab
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-06-30 11:06:50 -0300

    Se elimina el boton buscar

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -4,26 +4,21 @@ import PropTypes from 'prop-types';
 const SearchForm = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSearch(searchTerm);
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+    onSearch(e.target.value);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <div className="input-group mb-3">
         <input
           type="text"
           className="form-control"
           placeholder="Buscar películas"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleInputChange}
         />
-        <div className="input-group-append">
-          <button className="btn btn-primary" type="submit">
-            Buscar
-          </button>
-        </div>
       </div>
     </form>
   );
