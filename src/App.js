3cdf232ef1f23e3936629c2e53b11eed6a994515
Valentino Chiappanni <101267119+ValentinoChiappanni@users.noreply.github.com>
2023-06-30 10:38:17 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1>Repo semilla</h1>
+    </div>
+  );
+}
+
+export default App;
