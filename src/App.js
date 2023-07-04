1d9b227d96cf4afc262ef221612b725a87c39329
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-07-04 12:57:02 -0300

    Nuevo render

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/parcial2CIU" element={<Peliculas />} />
-        <Route path="/parcial2CIU-QuienesSomos" element={<QuienesSomos />} />
+        <Route path="/parcial2CIU/QuienesSomos" element={<QuienesSomos />} />
       </Routes>
     </>
   );
