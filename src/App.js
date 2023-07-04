35e2a643d0c1142bc1191998124472319c0556d7
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-07-04 12:39:26 -0300

    quienes somos y rutas

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import QuienesSomos from './components/QuienesSomos';
 
 function App() {
   return (
-    <Router>
+    <>
       <Routes>
         <Route path="/parcial2CIU" element={<Peliculas />} />
         <Route path="/parcial2CIU/QuienesSomos" element={<QuienesSomos />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
